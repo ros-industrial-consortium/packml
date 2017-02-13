a7374f2bfbabdbb5cfff228435bfe7959aa8faa0
Shaun Edwards <shaun.edwards@gmail.com>
2017-02-13 17:36:33 -0600

    Added wait for QCore application to unit test

--- packml_sm/test/utest.cpp
+++ packml_sm/test/utest.cpp
@@ -8,6 +8,7 @@
 #include <QCoreApplication>
 #include <QTimer>
 #include <boost/thread/thread.hpp>
+#include <ros/console.h>
 
 void qtWorker(int argc, char *argv[])
 {
@@ -19,6 +20,10 @@ int main(int argc, char **argv)
 {
   boost::thread* thr = new boost::thread(&qtWorker, argc, argv);
   ros::Time::init();
+  while(NULL == QCoreApplication::instance())
+  {
+    ROS_INFO_STREAM_THROTTLE(0.1, "Waiting for QCore application to start");
+  }
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
